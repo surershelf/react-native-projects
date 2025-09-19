0e959effe092b6d342bd55f5848ab19b1462f3de
mazzo <mazzolucas0@gmail.com>
2025-09-19 10:43:35 -0300

    finalizando o projeto

--- drawer-navigation/App.js
+++ drawer-navigation/App.js
@@ -1,20 +1,47 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { createDrawerNavigator } from "@react-navigation/drawer";
+import { NavigationContainer } from "@react-navigation/native";
+import { StyleSheet, Text, View } from "react-native";
+
+const Drawer = createDrawerNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Drawer.Navigator initialRouteName="Casa">
+        <Drawer.Screen name="Casa" component={HomeScreen} />
+        <Drawer.Screen name="Perfil" component={ProfileScreen} />
+      </Drawer.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  screen: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  text: {
+    textAlign: "center",
+    color: "black",
+    fontFamily: "arial",
+    fontSize: 20,
   },
 });
+
+function HomeScreen() {
+  return (
+    <View style={styles.screen}>
+      <Text style={styles.text}>CASAAAAAAAAA TELAAAA </Text>
+    </View>
+  );
+}
+
+function ProfileScreen() {
+  return (
+    <View style={styles.screen}>
+      <Text style={styles.text}> PERFIL TELAAAA</Text>
+    </View>
+  );
+}
--- stack-navigation/App.js
+++ stack-navigation/App.js
@@ -1,20 +1,65 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from "@react-navigation/native";
+import { StyleSheet, Text, View } from "react-native";
+import { Button } from "react-native-web";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
+const Stack = createNativeStackNavigator();
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Casa">
+        <Stack.Screen name="Casa" component={HomeScreen} />
+        <Stack.Screen name="Detalhes" component={DetailsScreen} />
+        <Stack.Screen name="Perfil" component={ProfileScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  screen: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  text: {
+    textAlign: "center",
+    color: "black",
+    fontFamily: "arial",
+    fontSize: 20,
   },
 });
+
+function HomeScreen({ navigation }) {
+  return (
+    <View style={styles.screen}>
+      <Text style={styles.text}>CASAAAAAAAAAAAAAAA TELAAAAAAAAAAAAAAAA</Text>
+      <Button
+        title="Ir para detalhes"
+        onPress={() => navigation.navigate("Detalhes", { produtoId: 101 })}
+      />
+    </View>
+  );
+}
+function DetailsScreen({ route, navigation }) {
+  const { produtoId } = route.params;
+  return (
+    <View style={styles.screen}>
+      <Text style={styles.text}> TELLLLAAAAAA DE PRODUTOOOSSSS</Text>
+      <Text> ID do produto: {produtoId}</Text>
+      <Button
+        title="Ir para o perfil"
+        onPress={() => navigation.navigate("Perfil")}
+      />
+    </View>
+  );
+}
+
+function ProfileScreen() {
+  return (
+    <View style={styles.screen}>
+      <Text style={styles.text}>PERFILLLLLLLLLLLLLLLLLLLLLL</Text>
+    </View>
+  );
+}
